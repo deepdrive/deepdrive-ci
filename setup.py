5d0055850a97d9c5f357883c54feb3006e8e9fca
Adam Rehn <adam.rehn@my.jcu.edu.au>
2019-05-06 13:03:20 +1000

    Bump version to 0.0.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(abspath(join(dirname(__file__), 'README.md')), 'rb') as readmeFile:
 
 setup(
 	name='deepdrive-ci',
-	version='0.0.4',
+	version='0.0.5',
 	description='Common code for Deepdrive CI scripts',
 	long_description=__readme__,
 	long_description_content_type='text/markdown',
